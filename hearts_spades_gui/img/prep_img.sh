76550222c9630e1f95f9719b8180bb5ed583bc89
Ryan Frazier <pianomanfrazier@gmail.com>
2016-11-05 14:06:03 -0600

    add img script and process images

--- hearts_spades_gui/img/prep_img.sh
+++ hearts_spades_gui/img/prep_img.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-INDIR=$(ls -d Classic/*/)
-#rename all files in dir tree
-for i in $INDIR
-do
-	rename 's/[cdsh]0?(.{1,2}\.png)$/$1/' $i*
-done
-
-#IMG=$(ls Classic/*/*.png)
-#
-#for i in $IMG
+#INDIR=$(ls -d Cards/*/)
+##rename all files in Cards folder
+#for i in $INDIR
 #do
-#	echo $i
-#	#convert -scale %15 +repage $IN $OUT
+#	rename 's/[cdsh]0?(.{1,2}\.png)$/$1/' $i*
 #done
+
+#echo "scale images to %15..."
+#for i in clubs spades diamonds hearts
+#do
+#	for j in $(ls Cards/$i) #get the card name
+#	do
+#		echo "Scaling Cards/$i/$j to scaled/$i/$j"
+#		convert -scale %15 +repage Cards/$i/$j scaled/$i/$j
+#	done
+#done
+
+echo "cropping scaled cards..."
+for i in clubs spades diamonds hearts
+do
+	for j in $(ls scaled/$i) #get the card name
+	do
+		echo "cropping scaled/$i/$j to scaled/$i/$j"
+		convert -gravity East -chop 75%x0 +repage scaled/$i/$j slice/$i/$j  
+	done
+done
